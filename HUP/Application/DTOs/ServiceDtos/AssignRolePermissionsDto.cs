b3cbf4bddbc220c013b3d8e40e46d130722916d3
Menna Magdy <menna11hassanien@gmail.com>
2025-11-18 23:41:00 +0200

    remove non-compatible DTOs

--- HUP/Application/DTOs/ServiceDtos/AssignRolePermissionsDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HUP.Core.DTOs.ServiceDtos
-{
-    public class AssignRolePermissionsDto
-    {
-        public int RoleId { get; set; }
-        public List<int> PermissionIds { get; set; }
-        public int PageId { get; set; }
-    }
-}
--- HUP/Application/DTOs/UserDtos/UpdateUserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HUP.Core.DTOs.UserDtos
-{
-    public class UpdateUserDto
-    {
-        public string FullName { get; set; }
-        public string Phone { get; set; }
-        public bool IsActive { get; set; }
-    }
-}
