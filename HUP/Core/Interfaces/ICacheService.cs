659ce1f89e8daf1575e819fa15438b9b68f5fe4c
Menna Magdy <menna11hassanien@gmail.com>
2025-11-18 21:44:51 +0200

    docker-compose.yml (redis container)/ CacheService (implementing basic methods for caching (not tested))

---
+++ HUP/Core/Interfaces/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace HUP.Core.Interfaces
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T>(string key);
+        Task SetAsync<T>(string key, T value, int expirationInMinutes);
+        Task RemoveAsync(string key);
+    }
+}
--- HUP/Program.cs
+++ HUP/Program.cs
@@ -1,18 +1,27 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using HUP.Data;
+using MongoDB.Driver;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
+
 
 builder.Services.AddDbContext<HUPDbContext>(options =>
     options.UseSqlServer(connectionString)
 );
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnectionString));
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
 
 var app = builder.Build();
 
