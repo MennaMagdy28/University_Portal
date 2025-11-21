3d219040400a9bf19eb6392af55036fd41ea9dbd
Omar Shalaby <ahero2004@gmail.com>
2025-11-22 00:30:55 +0200

    swagger fix

--- HUP/Program.cs
+++ HUP/Program.cs
@@ -8,22 +8,21 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<HUPDbContext>(options =>
     options.UseSqlServer(connectionString)
 );
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
