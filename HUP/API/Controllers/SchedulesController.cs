c6f556312d547d9dbc4f6fd7a57c118f12d890f6
Menna Magdy <menna11hassanien@gmail.com>
2026-01-21 15:21:34 +0200

    ??

---
+++ HUP/API/Controllers/SchedulesController.cs
@@ -0,0 +1,14 @@
+﻿using HUP.Application.DTOs.AcademicDtos;
+using HUP.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HUP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+        
+    }
+}
